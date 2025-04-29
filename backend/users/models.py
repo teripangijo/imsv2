d526a67fb64a6c237023be2b4e760a53ab8c426f
Etherman Nainggolan <ethermannainggolan@Ethermans-MacBook-Air.local>
2025-04-29 10:48:56 +0700

    Add models for users control

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    class Role(models.TextChoices):
+        PEMINTA = 'PEMINTA', _('Peminta')
+        ATASAN_PEMINTA = 'ATASAN_PEMINTA', _('Atasan Peminta')
+        OPERATOR = 'OPERATOR', _('Operator')
+        ATASAN_OPERATOR = 'ATASAN_OPERATOR', _('Atasan Operator')
+        ADMIN = 'ADMIN', _('Admin')
+
+    # Field username bawaan tidak digunakan, gunakan email sebagai username
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+
+    role = models.EmailField(_('Role'), choices=Role.choices, default=Role.PEMINTA, max_length=20)
+    departement_code = models.CharField(_('kode bagian/bidang'), max_length=10, blank=True, null=True)
+    password_reset_required = models.BooleanField(_('wajib ganti password'), default=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return self.get_full_name() or self.email
+    
+    @property
+    def is_peminta(self):
+        return self.role == self.Role.PEMINTA
+    
+    @property
+    def is_atasan_peminta(self):
+        return self.role == self.Role.ATASAN_PEMINTA
+    
+    @property
+    def is_operator(self):
+        return self.role == self.Role.OPERATOR
+    
+    @property
+    def is_atasan_operator(self):
+        return self.role == self.Role.ATASAN_OPERATOR
+    
+    @property
+    def is_admin(self):
+        return self.role == self.Role.ADMIN or self.is_superuser
