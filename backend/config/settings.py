39045839c0564af32d90148d40fffd5e0dee6fd9
Etherman Nainggolan <ethermannainggolan@Ethermans-MacBook-Air.local>
2025-04-30 13:53:19 +0700

    Add CORS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -136,6 +136,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://127.0.0.1:5173",
+    "http://localhost:5173",
 ]
 
 # Agar lebih permissif
